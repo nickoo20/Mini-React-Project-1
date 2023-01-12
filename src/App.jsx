080ffd1983bc181fe47805b9eb9abbf0fa100ed9
Niyati Gupta <111230452+nickoo20@users.noreply.github.com>
2023-01-12 22:31:44 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Main from './component/Main'
+
+const App = () => {
+  return (
+    <div className = 'container'>
+      <h1><Navbar/></h1>
+      <h2><Main/></h2>
+    </div>
+  )
+}
+
+export default App ;
+
+
+
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+export default function Navbar(){
+    return (
+        <nav>
+            <img src = "logo192.png" alt = "error" className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>   
+    )
+}
+
+
+
+
